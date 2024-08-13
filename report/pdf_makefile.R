5e0b82e86a21239defaebfb7357e72760a6b4f82
Paul Julian <pjulian@ufl.edu>
2024-08-13 08:23:07 -0400

    push update/changes

--- report/pdf_makefile.R
+++ report/pdf_makefile.R
@@ -1,6 +1,10 @@
 
-library(pagedown)
 path = "C:/Julian_LaCie/_GitHub/EvergladesConditions/report/"
+library(quarto)
+quarto_render(paste0(path,"LOK_PSC_figs.qmd"))
+
+
+library(pagedown)
 pdf.fname = paste0(format(Sys.Date(),"%Y%m%d"),"_LOKPSC.pdf")
 chrome_print(paste0(path,"LOK_PSC_figs.html"),
              paste0(path,"pdfs/",pdf.fname),
