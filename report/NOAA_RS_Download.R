0d442c57fe01e31a863070df88fe1477ff70ed4d
Paul Julian <pjulian@ufl.edu>
2024-09-04 08:26:45 -0400

    push

--- report/NOAA_RS_Download.R
+++ report/NOAA_RS_Download.R
@@ -47,9 +47,14 @@ noaa.image.inventory=cbind(link_txt,link_val)|>
   subset(product=="CIcyano")
 noaa.image.inventory$fname=with(noaa.image.inventory,paste0(product,"_",AOI,"_",format(date,"%Y%m%d"),".tif"))
 
+# noaa.image.inventory2=cbind(link_txt,link_val)|>
+#   subset(product=="truecolor")|>
+#   mutate(fname=paste0(product,"_",AOI,"_",format(date,"%Y%m%d"),".tif"))
 
 ## local file inventory
 local.image.inventory = data.frame(fname=list.files(data.path))
+local.image.inventory = subset(local.image.inventory,fname!="truecolor")
+
 str.val=strsplit(local.image.inventory$fname,"_|\\.")
 local.image.inventory$date=date.fun(paste(substr(sapply(str.val,"[",3),1,4),
                                           substr(sapply(str.val,"[",3),5,6),
@@ -59,9 +64,19 @@ local.image.inventory$date=date.fun(paste(substr(sapply(str.val,"[",3),1,4),
 max(noaa.image.inventory$date)
 max(local.image.inventory$date)
 
-## update for missing data
-noaa.image.inventory=subset(noaa.image.inventory,date>max(local.image.inventory$date))
+# local.image.inventory2 = data.frame(fname=list.files(paste0(data.path,"/truecolor")))
+# str.val=strsplit(local.image.inventory2$fname,"_|\\.")
+# local.image.inventory2$date=date.fun(paste(substr(sapply(str.val,"[",3),1,4),
+#                                           substr(sapply(str.val,"[",3),5,6),
+#                                           substr(sapply(str.val,"[",3),7,8),sep="-"))
+# 
+# 
+# max(noaa.image.inventory2$date)
+# max(local.image.inventory2$date)
 
+## update for missing data
+noaa.image.inventory=subset(noaa.image.inventory,date>max(local.image.inventory$date)); # CICyano
+# noaa.image.inventory2=subset(noaa.image.inventory2,date>max(local.image.inventory2$date)); # TrueColor
 ## data download  ------------------------------------------------
 
 if(nrow(noaa.image.inventory)!=0){
@@ -79,3 +94,15 @@ if(nrow(noaa.image.inventory)!=0){
   cat(paste0("\nData Downloaded/Update ",Sys.Date(),". ",nrow(noaa.image.inventory)," files downloaded."),file="./report/NOAADownloadLog.txt",append=T)
 }
 
+
+# if(nrow(noaa.image.inventory2)!=0){
+#   for(i in 1:nrow(noaa.image.inventory2)){
+#     
+#     url.link=noaa.image.inventory2$link[i]
+#     
+#     hd=httr::HEAD(url.link)
+#     if(hd$all_headers[[1]]$status!=200){next}else{
+#       try(download.file(url.link,paste(data.path,"truecolor", noaa.image.inventory2$fname[i],sep="/"),mode="wb",method="wininet"))
+#     }
+#   }
+# }
