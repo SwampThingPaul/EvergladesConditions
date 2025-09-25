1a7d7652e384578ef9d908973799d6fb06b28680
Paul Julian <pjulian@ufl.edu>
2025-09-25 14:22:36 -0400

    another attempt

--- pak setup.R
+++ pak setup.R
@@ -1,7 +1,7 @@
 
-libs_val <- c("remotes", "rmarkdown", "knitr", "reshape2", "plyr", "zoo", 
+libs_val <- c("remotes","markdown", "rmarkdown", "knitr", "reshape2", "plyr", "zoo", 
               "lubridate", "RcppRoll", "dataRetrieval", "downloadthis", "rvest", 
-              "httr", "mapmisc", "future", "future.apply", "flextable", "pak",
+              "httr", "mapmisc", "future", "future.apply", "flextable", "gdtools",
               "github::SwampThingPaul/AnalystHelper",
               "github::SwampThingPaul/EVERSpatDat")
 pak::lockfile_create(libs_val, dependencies = TRUE, lockfile = ".github/pkg.lock")
